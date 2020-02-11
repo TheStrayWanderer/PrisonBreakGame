31ac2213f68b8faa936ed778d7fcf2fe12b69b9f
TheStrayWanderer <60036063+TheStrayWanderer@users.noreply.github.com>
2020-02-11 22:26:05 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,15 +3,31 @@ import java.awt.*;
 public class Player extends GameObject{
 
 
-    public Player(int x, int y) {
-        super(x, y);
+    Handler handler;
 
-        velX = 1;
+    public Player(int x, int y, objectID id, Handler handler) {
+        super(x, y, id);
+        this.handler = handler;
     }
 
     public void tick() {
+
         x += velX;
         y += velY;
+
+        //Player Movement
+        if(handler.isUp()) velY = -3;
+        else if(!handler.isDown()) velY = 0;
+
+        if(handler.isDown()) velY = 3;
+        else if(!handler.isUp()) velY = 0;
+
+        if(handler.isRight()) velX = 3;
+        else if(!handler.isLeft()) velX = 0;
+
+        if(handler.isLeft()) velX = -3;
+        else if(!handler.isRight()) velX = 0;
+
     }
 
     public void render(Graphics g) {
@@ -20,6 +36,6 @@ public class Player extends GameObject{
     }
 
     public Rectangle getBounds() {
-        return null;
+        return new Rectangle(x, y, 10, 10);
     }
 }
