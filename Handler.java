d7dff184bcd0028bf7fe840421d817005a231189
TheStrayWanderer <60036063+TheStrayWanderer@users.noreply.github.com>
2020-02-11 22:29:29 +0000

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -3,7 +3,45 @@ import java.util.LinkedList;
 
 public class Handler {
 
+    /*Creates a list that links all Game Objects allowing
+    easier control of creation and removal of objects.
+     */
     LinkedList<GameObject> object = new LinkedList<GameObject>();
+
+    //Player movement boolean values
+    private boolean up = false, down = false, right = false, left = false;
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
 
     public void tick(){
         for(int i = 0; i < object.size(); i++){
