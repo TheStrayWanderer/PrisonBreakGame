fd82927d9838e7454b121aa0d6d9bb18c5d98a81
TheStrayWanderer <60036063+TheStrayWanderer@users.noreply.github.com>
2020-02-12 23:26:27 +0000

    Update objectID.java

--- objectID.java
+++ objectID.java
@@ -1,12 +1,14 @@
 public enum objectID {
-    /*
+	
+	/*
     This Enumeration was made in order to separate the different
     entities in the game apart to allow for interactions
     and collisions
      */
     Player(),
     Guard(),
-    Door(),
+    Block(),
     Gun(),
     Key();
+	
 }
