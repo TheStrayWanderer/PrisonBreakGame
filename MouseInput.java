6aa45f3366653669d1a34d4c5d022f40e9b7b1a0
TheStrayWanderer <60036063+TheStrayWanderer@users.noreply.github.com>
2020-02-14 12:06:51 +0000

    Update MouseInput.java

--- MouseInput.java
+++ MouseInput.java
@@ -1,5 +1,30 @@
-package prisonbreakgame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class MouseInput {
-
+public class MouseInput extends MouseAdapter{
+    
+    Handler handler;
+    
+    public MouseInput(Handler handler){
+        this.handler = handler;
+    }
+    
+    
+    public void MousePressed(MouseEvent e){
+        
+        int button = e.getButton();
+        
+        for (int i = 0; i < handler.object.size(); i++){
+            
+            GameObject tempObject = handler.object.get(i);
+            
+            if (tempObject.getId() == objectID.Player){
+                
+            }
+            
+        }
+        
+    }
+    
+    
 }
