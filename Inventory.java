5d270928a975a734cdde93a9906ff41be05e830e
TheStrayWanderer <60036063+TheStrayWanderer@users.noreply.github.com>
2020-02-14 12:05:53 +0000

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,11 @@
-package prisonbreakgame;
-
 public class Inventory {
-
+    
+    Handler handler;
+    
+    public Inventory(Handler handler){
+        
+        this.handler = handler;
+        
+    }
+    
 }
